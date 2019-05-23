ea4f3ad43def6fdbc6ee72e8e1554ecdd0f90095
phamquanganh31101998 <quanganhpham31101998@gmail.com>
2019-05-23 20:21:09 +0700

    Khung project Android

---
+++ app/src/main/java/com/example/weighttracker/Diet.java
@@ -0,0 +1,12 @@
+package com.example.weighttracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Diet extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diet);
+    }
+}
